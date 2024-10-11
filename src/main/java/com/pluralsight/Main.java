5a6e51d2c8c5cd6e5b1d033f22fd824eb021e726
Dee S <dsmallwood0427@gmail.com>
2024-10-11 15:36:16 -0500

    added transaction file

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,7 +1,33 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
+    static Scanner commandScanner = new Scanner(System.in);
+    static Scanner inputScanner = new Scanner(System.in);
+    static ArrayList<Transaction> = allTransaction = new ArrayList<>();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        loadTransactionFromFile();
+
+// Initialize the command variable (int mainMenuCommand;)
+        int mainMenuCommand;
+
+
+
     }
+
+
+
+
+
+
+
+
 }
---
+++ src/main/java/com/pluralsight/Transaction.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+public class Transaction {
+
+    // properties
+    private String date;
+    private String time;
+    private String description;
+    private String vendor;
+    private float amount;
+
+    // constructor
+    public Transaction(String date, String time, String description, String vendor, float amount) {
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
