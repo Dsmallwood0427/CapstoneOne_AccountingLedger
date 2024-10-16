a6a81846109e2eefd56004ff68866d7f13026462
Dee S <dsmallwood0427@gmail.com>
2024-10-16 10:24:42 -0500

    Added to Menu

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -19,7 +19,14 @@ public class Main {
 // Initialize the command variable (int mainMenuCommand;)
         int mainMenuCommand;
 
-
+        // Create a do-while loop with a conditional for the command=
+        do {
+            // Display the menu, sout
+            System.out.println("Please enter an option: ");
+            System.out.println("D) Add Deposit");
+            System.out.println("P) Make a Payment(Debit)");
+            System.out.println("L) Ledger");
+            System.out.print("X) Exit");
 
     }
 
--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -19,6 +19,82 @@ public class Transaction {
 
     }
 
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+
+        public String getDateOfTransactiom () {
+            return getDateOfTransactiom();
+        }
+
+        public void setDateOfTransaction (String dateOfTransaction){
+            this.dateOfTransaction = dateOfTransaction;
+        }
+
+        public String getTimeOfTansaction () {
+            return timeOfTransaction;
+        }
+
+        public void setTimeOfTransaction (String timeOfTransaction){
+            this.timeOfTransaction = timeOfTransaction;
+        }
+
+        public String getAmountOfTransaction () {
+            return AmountOfTransaction;
+        }
+
+        public void setAmountOfTransaction (String timeOfTransaction){
+            this.amountOfTransaction = amountOfTransaction;
+        }
+
+        @Override
+        public String toString () {
+            return "Transaction{" +
+                    "date='" + date + '\'' +
+                    ", description='" + description + '\'' +
+                    ", vendor=" + vendor +
+                    ", amount='" + amount + '\'' +
+                    ", dateOfTransaction='" + dateOfTransaction + '\'' +
+                    ", timeOfTransactions='" + timeOfTransaction + '\'' +
+                    ", amountOfTransactions='" + amountOfTransaction + '\'' +
+                    '}';
+        }
+    }
 
 
 
